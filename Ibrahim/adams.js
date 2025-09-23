0bddc3e79a0695515bb362a99c155d388503c605
xason0 <xasongab@gmail.com>
2025-09-23 16:32:09 +0000

    Added Ibrahim helper and adams modules

---
+++ Ibrahim/adams.js
@@ -0,0 +1,5 @@
+module.exports = {
+  start: () => console.log("[Adams] start called"),
+  stop: () => console.log("[Adams] stop called"),
+  info: () => console.log("[Adams] module loaded"),
+};
---
+++ Ibrahim/helper.js
@@ -0,0 +1,5 @@
+module.exports = {
+  log: (msg) => console.log("[Helper]", msg),
+  error: (msg) => console.error("[Helper Error]", msg),
+  success: (msg) => console.log("[Helper Success]", msg),
+};
